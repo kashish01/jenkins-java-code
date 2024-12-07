b7a06b5a5929d1f8146dace4809b1f1eeaa73b7a
Kashish Gaba <36818461+kashish01@users.noreply.github.com>
2024-12-07 11:09:22 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+import java.util.Date;
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("java program by KAshish");
+        System.out.println("java program output");
+        System.out.println(new Date());
+    }
+}
